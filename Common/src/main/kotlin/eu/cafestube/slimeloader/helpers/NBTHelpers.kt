25d544441d3327a3779c2322f3b784fc9b2a9cc0
David <davidliebtkekse@gmail.com>
2026-07-23 21:22:12 +0200

    fix: readNbtTag using bufferedinputstream

--- Common/src/main/kotlin/eu/cafestube/slimeloader/helpers/NBTHelpers.kt
+++ Common/src/main/kotlin/eu/cafestube/slimeloader/helpers/NBTHelpers.kt
@@ -4,7 +4,10 @@ package eu.cafestube.slimeloader.helpers
 import net.kyori.adventure.nbt.BinaryTag
 import net.kyori.adventure.nbt.BinaryTagIO
 import java.io.ByteArrayInputStream
+import java.io.DataInput
+import java.io.DataInputStream
 
 
+//See https://github.com/InfernalSuite/AdvancedSlimePaper/pull/194 why we cast to DataInput and use DataInputStream here
 inline fun <reified T : BinaryTag> readNBTTag(bytes: ByteArray): T?
-        = BinaryTagIO.reader(Long.MAX_VALUE).read(ByteArrayInputStream(bytes), BinaryTagIO.Compression.NONE) as? T
+        = BinaryTagIO.reader(Long.MAX_VALUE).read(DataInputStream(ByteArrayInputStream(bytes)) as DataInput) as? T
